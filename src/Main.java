76ef99373082f60584b1086c1ee6f3ec758b2b75
Kristina Semenova <semenova.kristina.00@mail.ru>
2021-08-13 15:55:52 +0300

    src/Main.java added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 15010;
+        int miles = 20;
+        int amount = price / miles;
+
+        System.out.println(amount);
+    }
+}
